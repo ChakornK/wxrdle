6edaccdcc91e191aff55a50104b556d99e587b6a
ChakornK <chakornk2007@gmail.com>
2025-12-26 12:44:57 -0800

    feat: add solver function

---
+++ src/lib/solver.ts
@@ -0,0 +1,93 @@
+import { WORDS } from "../data/wordle/words";
+
+type Constraints = {
+  absent: string[];
+  present: string[];
+  incorrect: Record<number, string[]>;
+  correct: Record<number, string>;
+};
+
+export const nextWord = (constraints: Constraints) => {
+  let w = WORDS.filter(
+    (word) => !constraints.absent.some((l) => word.includes(l))
+  );
+  if (constraints.present.length > 0) {
+    w = w.filter((word) => constraints.present.every((l) => word.includes(l)));
+  }
+  w = w
+    .filter((word) =>
+      word
+        .split("")
+        .every((l, i) => !(constraints.incorrect[i] || []).includes(l))
+    )
+    .filter((word) =>
+      word
+        .split("")
+        .every((l, i) =>
+          constraints.correct[i] ? constraints.correct[i] === l : true
+        )
+    );
+
+  const freq = letterFreq(w);
+
+  const lettersByFreq = Object.entries(freq).filter(
+    ([l]) => !constraints.present.includes(l)
+  );
+
+  const candidates = (constraints.present.length > 2 ? w : WORDS).filter(
+    (word) => word.split("").some((l) => lettersByFreq.find(([lf]) => lf === l))
+  );
+  const candidatesRanked = candidates
+    .map((word) => {
+      return [
+        word,
+        [...new Set(word.split(""))]
+          .map(
+            (l) => (lettersByFreq.find(([lf]) => lf === l) || [0, -1])[1] + 1
+          )
+          .reduce((a, b) => a + b, 0),
+      ];
+    })
+    .sort((a: [string, number], b: [string, number]) => b[1] - a[1]) as [
+    string,
+    number
+  ][];
+
+  const maxScore = candidatesRanked.reduce((a, b) => Math.max(a, b[1]), 0);
+
+  const bestCandidates = candidatesRanked.filter((c) => c[1] === maxScore);
+
+  if (bestCandidates.length == 1) return bestCandidates[0][0];
+
+  const allWordsLetterFreq = letterFreq(WORDS);
+  const bestCandidateByFreq = bestCandidates
+    .map(([word]) => word)
+    .map((word) => {
+      return [
+        word,
+        [...new Set(word.split(""))]
+          .map(
+            (l) =>
+              (Object.entries(allWordsLetterFreq).find(([lf]) => lf === l) || [
+                0, -1,
+              ])[1] + 1
+          )
+          .reduce((a, b) => a + b, 0),
+      ];
+    })
+    .sort((a: [string, number], b: [string, number]) => b[1] - a[1]) as [
+    string,
+    number
+  ][];
+  return bestCandidateByFreq[0][0];
+};
+
+const letterFreq = (words: string[]) => {
+  const freq: Record<string, number> = {};
+  for (const word of words) {
+    for (const letter of word.split("")) {
+      freq[letter] = (freq[letter] || 0) + 1;
+    }
+  }
+  return freq;
+};
