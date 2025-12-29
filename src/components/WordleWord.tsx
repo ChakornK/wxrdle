5edbb77aab14396d411f6f9c97e541c4227e9c88
ChakornK <chakornk2007@gmail.com>
2025-12-28 20:21:38 -0800

    feat: letter type animation

--- src/components/WordleWord.tsx
+++ src/components/WordleWord.tsx
@@ -47,27 +47,50 @@ export const WordleWord = ({ guess }: { guess: WordleGuess }) => {
       {Array.from({ length: 5 })
         .map((_, i) => guess[i] || { letter: "", status: LetterStatus.pending })
         .map((letter, i) => (
-          <div key={i} class="relative h-[2em] w-[2em] font-bold uppercase">
-            <div
-              class={`letter-back absolute inset-0 flex items-center justify-center ${
-                variants[letter.status]
-              }`}
-            >
-              {letter.letter}
-            </div>
-            <div
-              class={`letter-front absolute inset-0 flex items-center justify-center ${
-                variants[
-                  letter.letter == ""
-                    ? LetterStatus.pending + 1
-                    : LetterStatus.pending
-                ]
-              }`}
-            >
-              {letter.letter}
-            </div>
-          </div>
+          <WordleLetter key={i} letter={letter.letter} status={letter.status} />
         ))}
+    </div>
+  );
+};
+
+const WordleLetter = ({
+  letter,
+  status,
+}: {
+  letter: string;
+  status: LetterStatus;
+}) => {
+  const [scope, animate] = useAnimate();
+  const [typed, setTyped] = useState<boolean>(
+    status === LetterStatus.pending + 1
+  );
+
+  useEffect(() => {
+    if (status === LetterStatus.pending && letter !== "") {
+      animate(
+        scope.current,
+        { scale: [1, 1.2, 1] },
+        { duration: 0.1, times: [0, 0.3, 1] }
+      );
+    }
+  }, [letter]);
+
+  return (
+    <div class="relative h-[2em] w-[2em] font-bold uppercase" ref={scope}>
+      <div
+        class={`letter-back absolute inset-0 flex items-center justify-center ${variants[status]}`}
+      >
+        {letter}
+      </div>
+      <div
+        class={`letter-front absolute inset-0 flex items-center justify-center ${
+          variants[
+            letter == "" ? LetterStatus.pending + 1 : LetterStatus.pending
+          ]
+        }`}
+      >
+        {letter}
+      </div>
     </div>
   );
 };
